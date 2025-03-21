c85d7dea2af21ff9318bebaa82c7f9d703d7691e
guythestampede <guythestampede@gmail.com>
2025-03-21 17:16:10 -0500

    creating textnode.py file with an Enum and textnode class

---
+++ src/main.py
@@ -0,0 +1,6 @@
+from textnode import *
+
+print ("hello world")
+Node1 = TextNode("This is some anchor text", "link", "https://www.boot.dev")
+Node2 = TextNode("This is some anchor text", "link", "https://www.boot.dev")
+print (repr(Node1))
